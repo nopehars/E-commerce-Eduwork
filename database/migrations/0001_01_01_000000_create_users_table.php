c8657d31dccfb77f7db010bae7ec019e0d146c02
iqbaldarusallam <iqbaldarusallam@gmail.com>
2025-12-04 19:58:53 +0700

    change setup tailwind

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at');
             $table->string('password');
-            $table->string('phone')->nullable();
+            $table->string('phone');
             $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
