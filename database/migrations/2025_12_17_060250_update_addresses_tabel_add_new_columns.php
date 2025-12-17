04c7ab983933ef4ec79632c76a96e89a94b2f81a
irfan-fauzi <irfanwintech@yahoo.com>
2025-12-17 13:42:44 +0700

    add address column

---
+++ database/migrations/2025_12_17_060250_update_addresses_tabel_add_new_columns.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->string('recipient_name')->nullable()->after('label');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            if (Schema::hasColumn('addresses', 'recipient_name')) {
+              $table->dropColumn('recipient_name');
+            }
+        });
+    }
+};
